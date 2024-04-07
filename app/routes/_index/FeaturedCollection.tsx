e7c9c9c808e24fba6c941732a0698be6f6d407ac
RyotoNoguchi <ryoto_noguchi@wamazing.jp>
2024-04-07 23:26:46 +0900

    ♻️ _index.tsxをArrowFunctionalComponentに変更して、子コンポーネントを外だし。

---
+++ app/routes/_index/FeaturedCollection.tsx
@@ -0,0 +1,24 @@
+import { Link } from '@remix-run/react'
+import { Image } from '@shopify/hydrogen'
+import React from 'react'
+
+import type { FeaturedCollectionFragment } from 'storefrontapi.generated'
+
+type Props = {
+  collection: FeaturedCollectionFragment
+}
+
+export const FeaturedCollection: React.FC<Props> = ({ collection }) => {
+  if (!collection) return null
+  const image = collection?.image
+  return (
+    <Link className="featured-collection" to={`/collections/${collection.handle}`}>
+      {image && (
+        <div className="featured-collection-image">
+          <Image data={image} sizes="100vw" />
+        </div>
+      )}
+      <h1>{collection.title}</h1>
+    </Link>
+  )
+}
---
+++ app/routes/_index/route.tsx
@@ -0,0 +1,83 @@
+import { useLoaderData, type MetaFunction } from '@remix-run/react'
+import { defer, type LoaderFunctionArgs } from '@shopify/remix-oxygen'
+import { FeaturedCollection } from './FeaturedCollection'
+import { RecommendedProducts } from './RecommendedProducts'
+
+export const meta: MetaFunction = () => {
+  return [{ title: 'Hydrogen | Home' }]
+}
+
+export async function loader({ context }: LoaderFunctionArgs) {
+  const { storefront } = context
+  const { collections } = await storefront.query(FEATURED_COLLECTION_QUERY)
+  const featuredCollection = collections.nodes[0]
+  const recommendedProducts = storefront.query(RECOMMENDED_PRODUCTS_QUERY)
+
+  return defer({ featuredCollection, recommendedProducts })
+}
+
+const Homepage = () => {
+  const data = useLoaderData<typeof loader>()
+  return (
+    <div className="home">
+      <FeaturedCollection collection={data.featuredCollection} />
+      <RecommendedProducts products={data.recommendedProducts} />
+    </div>
+  )
+}
+
+export default Homepage
+
+const FEATURED_COLLECTION_QUERY = `#graphql
+  fragment FeaturedCollection on Collection {
+    id
+    title
+    image {
+      id
+      url
+      altText
+      width
+      height
+    }
+    handle
+  }
+  query FeaturedCollection($country: CountryCode, $language: LanguageCode)
+    @inContext(country: $country, language: $language) {
+    collections(first: 1, sortKey: UPDATED_AT, reverse: true) {
+      nodes {
+        ...FeaturedCollection
+      }
+    }
+  }
+` as const
+
+const RECOMMENDED_PRODUCTS_QUERY = `#graphql
+  fragment RecommendedProduct on Product {
+    id
+    title
+    handle
+    priceRange {
+      minVariantPrice {
+        amount
+        currencyCode
+      }
+    }
+    images(first: 1) {
+      nodes {
+        id
+        url
+        altText
+        width
+        height
+      }
+    }
+  }
+  query RecommendedProducts ($country: CountryCode, $language: LanguageCode)
+    @inContext(country: $country, language: $language) {
+    products(first: 4, sortKey: UPDATED_AT, reverse: true) {
+      nodes {
+        ...RecommendedProduct
+      }
+    }
+  }
+` as const
