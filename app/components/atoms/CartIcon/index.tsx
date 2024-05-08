b697b46cda38813968d6788278cdf8e2aa48a25a
RyotoNoguchi <ryoto_noguchi@wamazing.jp>
2024-05-08 10:07:18 +0900

    CartIcon追加

---
+++ app/components/atoms/CartIcon/index.tsx
@@ -0,0 +1,8 @@
+export const CartIcon = () => {
+  return (
+    <svg width='48' height='48' viewBox='0 0 48 48' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <rect x='10' y='14' width='28' height='24' stroke='white' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' />
+      <path d='M16 14C16 9.58172 19.5817 6 24 6C28.4183 6 32 9.58172 32 14' stroke='white' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' />
+    </svg>
+  )
+}
