f40cc3bbfbbdf50939c7cc3af928558407509e19
RyotoNoguchi <ryoto_noguchi@wamazing.jp>
2024-07-19 22:10:22 +0900

    ✨ 受取日として選択する日付に曜日を追加

--- app/components/atoms/DateSelector/index.tsx
+++ app/components/atoms/DateSelector/index.tsx
@@ -20,18 +20,22 @@ export const DateSelector: React.FC<Props> = ({ onChange }) => {
     fetchHolidays()
   }, [])
 
-  // 日付を YYYY-MM-DD 形式に変換する関数
-  const formatDate = (date: Date) => {
-    return date.toISOString().split('T')[0]
+  // 曜日を日本語で表現するための配列
+  const daysOfWeek = ['日', '月', '火', '水', '木', '金', '土']
+
+  // 日付を YYYY-MM-DD 形式に変換し、曜日を追加する関数
+  const formatDateWithDay = (date: Date) => {
+    const formattedDate = date.toISOString().split('T')[0]
+    const dayOfWeek = daysOfWeek[date.getDay()]
+    return `${formattedDate} (${dayOfWeek})`
   }
 
-  // 翌々日から1週間の日付を生成
+  // 翌々日から1週間の日付を生成し、日曜日と祝日を排除
   const dates = Array.from({ length: 7 }, (_, i) => {
     const date = new Date(today)
     date.setDate(date.getDate() + i + 2) // 翌々日から開始するために +2
     return date
-    // 日曜日と祝日を除外
-  }).filter((date) => date.getDay() !== 0 && !holidays[formatDate(date)])
+  }).filter((date) => date.getDay() !== 0 && !holidays[date.toISOString().split('T')[0]])
 
   return (
     <select onChange={(e) => onChange(e.target.value)} className='w-full text-xs border border-gray-300 bg-slate-200 rounded-md text-black p-2'>
@@ -40,8 +44,8 @@ export const DateSelector: React.FC<Props> = ({ onChange }) => {
       </option>
       {dates.map((date, index) => (
         // eslint-disable-next-line react/no-array-index-key
-        <option key={index} value={formatDate(date)}>
-          {formatDate(date)}
+        <option key={index} value={date.toISOString().split('T')[0]}>
+          {formatDateWithDay(date)}
         </option>
       ))}
     </select>
